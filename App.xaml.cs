2f29c7740d4e8fdf6339cf81fd21ee3bb155b9af
Mikkel Kappel Persson <contact@mikkelkappelpersson.com>
2023-05-18 10:46:22 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Directors_Viewfinder;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Directors_Viewfinder;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
