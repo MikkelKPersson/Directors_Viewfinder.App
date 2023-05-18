929928785586e1cf91a559eb480c63e119090a6f
Mikkel Kappel Persson <contact@mikkelkappelpersson.com>
2023-05-18 14:24:46 +0200

    App working with camera2 camera view

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using Directors_Viewfinder.Platforms.Android;
 
 namespace Directors_Viewfinder;
 
@@ -9,7 +10,11 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
+            .ConfigureMauiHandlers(handlers =>
+            {
+                handlers.AddHandler<CameraView, CameraViewRenderer>();
+            })
+            .ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
