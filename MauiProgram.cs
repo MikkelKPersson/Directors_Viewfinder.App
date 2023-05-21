8b1caa24479ab12ae651ae1c3e967f04a5685ea6
Mikkel Kappel Persson <contact@mikkelkappelpersson.com>
2023-05-21 17:54:11 +0200

    Merge

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,13 +1,4 @@
 ﻿using Microsoft.Extensions.Logging;
-<<<<<<< HEAD
-<<<<<<< HEAD
-#if __ANDROID__
-using Directors_Viewfinder.Platforms.Android;
-#endif
-=======
->>>>>>> parent of 9299287 (App working with camera2 camera view)
-=======
->>>>>>> parent of 9299287 (App working with camera2 camera view)
 
 namespace Directors_Viewfinder;
 
@@ -18,21 +9,7 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
-<<<<<<< HEAD
-<<<<<<< HEAD
-            .ConfigureMauiHandlers(handlers =>
-            {
-#if __ANDROID__
-                handlers.AddHandler<CameraView, CameraViewRenderer>();
-#endif
-			})
-            .ConfigureFonts(fonts =>
-=======
 			.ConfigureFonts(fonts =>
->>>>>>> parent of 9299287 (App working with camera2 camera view)
-=======
-			.ConfigureFonts(fonts =>
->>>>>>> parent of 9299287 (App working with camera2 camera view)
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
