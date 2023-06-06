e55773ae564968cc86321434f00eaac4b9f15268
Mikkel Kappel Persson <contact@mikkelkappelpersson.com>
2023-06-06 21:48:32 +0200

    Added list of Focal Lengths

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,14 +1,18 @@
 ﻿#if __ANDROID__
 using Directors_Viewfinder.Platforms.Android.Camera;
 #endif
+using System.Collections.Generic;
+using Microsoft.Maui.Controls;
+
 namespace Directors_Viewfinder
 {
     public partial class MainPage : ContentPage
     {
 #if __ANDROID__
         private CameraStateManager _cameraStateManager;
-
 #endif
+        private List<int> _focalLengths = new List<int> { 24, 35, 50, 85 };
+        private int _currentFocalLengthIndex = 0;
 
         public MainPage()
         {
@@ -17,6 +21,11 @@ namespace Directors_Viewfinder
             _cameraStateManager = CameraStateManager.Instance;
 #endif
             SwitchCameraButton.Clicked += SwitchCameraButton_Clicked;
+
+            FocalLengthUpButton.Clicked += (s, e) => ChangeFocalLength(1);
+            FocalLengthDownButton.Clicked += (s, e) => ChangeFocalLength(-1);
+
+            UpdateFocalLengthLabel();
         }
 
         private void SwitchCameraButton_Clicked(object sender, EventArgs e)
@@ -24,6 +33,17 @@ namespace Directors_Viewfinder
 #if __ANDROID__
             _cameraStateManager.SwitchCamera();
 #endif
+        }
+
+        private void ChangeFocalLength(int direction)
+        {
+            _currentFocalLengthIndex = Math.Max(0, Math.Min(_focalLengths.Count - 1, _currentFocalLengthIndex + direction));
+            UpdateFocalLengthLabel();
+        }
+
+        private void UpdateFocalLengthLabel()
+        {
+            FocalLengthLabel.Text = $"{_focalLengths[_currentFocalLengthIndex]} mm";
         }
     }
 }
